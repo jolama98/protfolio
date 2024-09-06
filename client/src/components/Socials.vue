ecded5ce1d30f65aa9fa597bf7222ec64b8dc1c2
jo <josehehall98@gmail.com>
2024-09-06 12:43:28 -0600

    int

--- client/src/components/Socials.vue
+++ client/src/components/Socials.vue
@@ -1,11 +1,12 @@
 <template>
-	<div class="d-flex justify-content-center gap-4">
-		<span class="mb-2" v-for="link in socials">
-			<a :href="link.href" referrerpolicy="no-referrer" rel="noopener" :title="link.name">
-				<i class="fs-2" :class="link.icon"></i>
-			</a>
-		</span>
-	</div>
+  <div class="d-flex justify-content-center gap-4">
+    <span class="mb-2" v-for="link in socials">
+      <a href="https://www.linkedin.com/in/joseph-hall-ab874a262/" referrerpolicy="no-referrer" rel="noopener"
+        :title="link.name">
+        <i class="fs-2" :class="link.icon"></i>
+      </a>
+    </span>
+  </div>
 </template>
 
 <script>
@@ -13,11 +14,11 @@ import { computed } from 'vue';
 import { AppState } from '../AppState.js';
 
 export default {
-	setup() {
-		return {
-			socials: computed(() => AppState.socialLinks)
-		}
-	}
+  setup() {
+    return {
+      socials: computed(() => AppState.socialLinks)
+    }
+  }
 }
 </script>
 
